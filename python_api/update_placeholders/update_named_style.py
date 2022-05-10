1113af13d65cea7844eb9a4317fb0af071e1b19b
hexu <hexu_1985@sina.com>
2022-05-10 18:19:53 +0800

    add update_placeholders

---
+++ python_api/update_placeholders/update_named_style.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cur = conn.cursor()
+print ("数据库打开成功")
+
+cur.execute("UPDATE COMPANY set SALARY = :salary where ID=:id", {"id": 1, "salary": 25000})
+conn.commit()
+print("Total number of rows updated : {}".format(conn.total_changes))
+
+cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
+for row in cursor:
+   print("ID = {}".format(row[0]))
+   print("NAME = {}".format(row[1]))
+   print("ADDRESS = {}".format(row[2]))
+   print("SALARY = {}".format(row[3]))
+   print("")
+
+print ("数据操作成功")
+conn.close()
