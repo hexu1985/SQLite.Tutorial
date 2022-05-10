3c962f1ee6c28b24fa342d1cdc6e983ff2e2cba5
hexu <hexu_1985@sina.com>
2022-05-10 14:56:38 +0800

    update create_table and insert_data

---
+++ python_api/README.md
@@ -0,0 +1,7 @@
+### python api
+
+#### 参考文档:
+
+- [SQLite - Python](https://www.runoob.com/sqlite/sqlite-python.html)
+- [Python sqlite3数据库模块使用攻略](https://zhuanlan.zhihu.com/p/196807781)
+- [sqlite3 — DB-API 2.0 interface for SQLite databases](https://docs.python.org/3/library/sqlite3.html)
--- python_api/create_table/create_table.py
+++ python_api/create_table/create_table.py
@@ -4,8 +4,8 @@ import sqlite3
 
 conn = sqlite3.connect('test.db')
 print ("数据库打开成功")
-c = conn.cursor()
-c.execute('''CREATE TABLE COMPANY
+cur = conn.cursor()
+cur.execute('''CREATE TABLE COMPANY
        (ID INT PRIMARY KEY     NOT NULL,
        NAME           TEXT    NOT NULL,
        AGE            INT     NOT NULL,
