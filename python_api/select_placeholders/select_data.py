ee83566ca11dac1d2932df9e516c8614f3281701
hexu <hexu_1985@sina.com>
2022-05-10 18:07:20 +0800

    add insert_placeholders and select_placeholders

---
+++ python_api/select_placeholders/select_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cur = conn.cursor()
+print ("数据库打开成功")
+
+cursor = cur.execute("SELECT id, name, address, salary from COMPANY where SALARY < :salary", {"salary": 30000})
+for row in cursor:
+   print("ID = {}".format(row[0]))
+   print("NAME = {}".format(row[1]))
+   print("ADDRESS = {}".format(row[2]))
+   print("SALARY = {}".format(row[3]))
+   print("")
+
+print ("数据操作成功")
+conn.close()
