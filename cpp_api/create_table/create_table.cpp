d72cf77d9680142da7db34408da63ba8276799ef
hexu <hexu_1985@sina.com>
2022-05-13 16:56:25 +0800

    add cpp_api/create_table/create_table2.cpp

--- cpp_api/create_table/create_table.cpp
+++ cpp_api/create_table/create_table.cpp
@@ -9,12 +9,14 @@ int main()
 {
     SQLite::Database db("test.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
     printf("数据库打开成功\n");
+
     db.exec(R"(CREATE TABLE COMPANY
        (ID INT PRIMARY KEY     NOT NULL,
        NAME           TEXT    NOT NULL,
        AGE            INT     NOT NULL,
        ADDRESS        CHAR(50),
        SALARY         REAL);)");
+
     printf("数据表创建成功\n");
 
     return 0;
---
+++ cpp_api/insert_data/insert_data2.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/Transaction.h>
+
+int main()
+{
+    SQLite::Database db("test.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+    printf("数据库打开成功\n");
+
+    {
+        SQLite::Transaction transaction(db);
+        db.exec("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+        
+        db.exec("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+        
+        db.exec("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+        
+        db.exec("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+        transaction.commit();
+
+        printf("数据插入成功\n");
+    }
+
+    return 0;
+}
+
