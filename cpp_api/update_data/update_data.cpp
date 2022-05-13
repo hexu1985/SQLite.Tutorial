d3d18467a999de44c294b3dc1a38e6052899ae6e
hexu <hexu_1985@sina.com>
2022-05-13 18:08:38 +0800

    add cpp_api/update_data

---
+++ cpp_api/update_data/update_data.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <SQLiteCpp/SQLiteCpp.h>
+
+int main()
+{
+    SQLite::Database db("test.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+    printf("数据库打开成功\n");
+
+    db.exec("UPDATE COMPANY set SALARY = 25000.00 where ID=1");
+
+    SQLite::Statement query(db, "SELECT id, name, address, salary from COMPANY");
+    while (query.executeStep()) {
+        std::cout << "ID = " << query.getColumn(0) << "\n";
+        std::cout << "NAME = " << query.getColumn(1) << "\n";
+        std::cout << "ADDRESS = " << query.getColumn(2) << "\n";
+        std::cout << "SALARY = " << query.getColumn(3) << "\n";
+        std::cout << std::endl;
+    }
+
+    printf("数据操作成功\n");
+
+    return 0;
+}
+
