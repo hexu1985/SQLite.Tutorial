56ee9e5259e08dd9b85939c36fdfd2e9e09425de
hexu <hexu_1985@sina.com>
2022-07-01 11:08:03 +0800

    add c_api/drop_table

---
+++ c_api/drop_table/drop_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main()
+{
+    sqlite3 *db;
+    char *zErr;
+    int rc;
+    char *sql;
+
+    rc = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    printf("数据库打开成功\n");
+
+    sql = "DROP TABLE COMPANY;";
+    rc = sqlite3_exec(db, sql, NULL, NULL, &zErr);
+
+    if (rc != SQLITE_OK) {
+        if (zErr != NULL) {
+            fprintf(stderr, "SQL error: %s\n", zErr);
+            sqlite3_free(zErr);
+        }
+    }
+    printf("数据表删除成功\n");
+
+    sqlite3_close(db);
+
+    return 0;
+}
+
