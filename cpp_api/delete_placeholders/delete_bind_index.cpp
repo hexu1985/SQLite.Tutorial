fde736f9d2a0f954769a259e3d305fc6f04b8806
hexu <hexu_1985@sina.com>
2022-07-01 15:32:50 +0800

    rename filename of delete_placeholders of c++ and python

---
+++ cpp_api/delete_placeholders/delete_bind_index.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/ExecuteMany.h>
+
+int main()
+{
+    SQLite::Database db("test.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+    printf("数据库打开成功\n");
+
+    SQLite::Statement qry(db, "DELETE from COMPANY where ID=?100;");
+    qry.bind(100, 1);
+    qry.exec();
+    printf("Total number of rows deleted : %d\n", qry.getChanges());
+
+    SQLite::Statement query(db, "SELECT id, name, address, salary  from COMPANY");
+    while (query.executeStep()) {
+        std::cout << "ID = " << query.getColumn(0) << "\n";
+        std::cout << "NAME = " << query.getColumn(1) << "\n";
+        std::cout << "ADDRESS = " << query.getColumn(2) << "\n";
+        std::cout << "SALARY = " << query.getColumn(3) << "\n";
+        std::cout << std::endl;
+    }
+
+    printf("数据操作成功\n");
+
+    return 0;
+}
+
