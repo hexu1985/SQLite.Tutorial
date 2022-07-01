3ee21951a9eeb40ab8bac18a7024965e8929dbee
hexu <hexu_1985@sina.com>
2022-07-01 11:50:43 +0800

    add c_api/select_data

--- c_api/select_data/select_data_callback.c
+++ c_api/select_data/select_data_callback.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
+int callback(void* data, int ncols, char** values, char** headers);
+
 int main()
 {
     sqlite3 *db;
     char *zErr;
     int rc;
     char *sql;
+	char* data;
 
     rc = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 
@@ -19,12 +22,10 @@ int main()
 
     printf("数据库打开成功\n");
 
-    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (1, 'Paul', 32, 'California', 20000.00 );"
-          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"
-          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"
-          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    data = "Callback function called";
+    sql = "SELECT id, name, address, salary from COMPANY;";
 
-    rc = sqlite3_exec(db, sql, NULL, NULL, &zErr);
+    rc = sqlite3_exec(db, sql, callback, data, &zErr);
 
     if (rc != SQLITE_OK) {
         if (zErr != NULL) {
@@ -32,7 +33,7 @@ int main()
             sqlite3_free(zErr);
         }
     } else {
-        printf("数据插入成功\n");
+        printf("数据操作成功\n");
     }
 
     sqlite3_close(db);
@@ -40,3 +41,14 @@ int main()
     return 0;
 }
 
+int callback(void* data, int ncols, char** values, char** headers)
+{
+    int i;
+    //fprintf(stderr, "%s: \n", (const char*)data);
+    for(i=0; i < ncols; i++) {
+        fprintf(stderr, "%s = %s\n", headers[i], values[i]);
+    }
+
+    fprintf(stderr, "\n");
+    return 0;
+}
