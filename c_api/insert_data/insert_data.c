63fc33fb3ad18410ada4ada844b8055714fd8ec7
hexu <hexu_1985@sina.com>
2022-07-01 11:21:24 +0800

    add c_api/insert_data

---
+++ c_api/insert_data/insert_data.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main()
+{
+    sqlite3 *db;
+    char *zErr;
+    int rc;
+    char *sql;
+
+    rc = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    printf("数据库打开成功\n");
+
+    sql = "CREATE TABLE COMPANY "
+          "(ID      INT     PRIMARY KEY   NOT NULL, "
+          "NAME     TEXT    NOT NULL, "
+          "AGE      INT     NOT NULL, "
+          "ADDRESS  CHAR(50), "
+          "SALARY   REAL);";
+    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (1, 'Paul', 32, 'California', 20000.00 );"
+          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"
+          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"
+          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+
+    rc = sqlite3_exec(db, sql, NULL, NULL, &zErr);
+
+    if (rc != SQLITE_OK) {
+        if (zErr != NULL) {
+            fprintf(stderr, "SQL error: %s\n", zErr);
+            sqlite3_free(zErr);
+        }
+    } else {
+        printf("数据插入成功\n");
+    }
+
+    sqlite3_close(db);
+
+    return 0;
+}
+
