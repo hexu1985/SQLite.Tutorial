dda8ea10053547e36f28bcaafda55bf5b25a0249
hexu <hexu_1985@sina.com>
2022-07-25 10:34:39 +0800

    update python/create_table/create_table.py add if not exists

--- api/python/create_table/create_table.py
+++ api/python/create_table/create_table.py
@@ -6,7 +6,7 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print ("数据库打开成功")
 cur = conn.cursor()
-cur.execute('''CREATE TABLE COMPANY
+cur.execute('''CREATE TABLE IF NOT EXISTS COMPANY 
        (ID INT PRIMARY KEY     NOT NULL,
        NAME           TEXT    NOT NULL,
        AGE            INT     NOT NULL,
